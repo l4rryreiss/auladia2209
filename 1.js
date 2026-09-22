27c47971d1ffc039ef9cfedc8809d44d80988069
౨ৎ ˖ ℒ𝒶𝓇𝓇𝓎 𝓒é𝓈𝒶𝓇 ˖ Ი ᰍ <larrymiaumiau@gmail.com>
2026-09-22 16:25:41 -0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+function calcularAreaRetangulo () {
+    let base = Number(prompt("Digite o valor da base:"))
+    let altura = Number(prompt("Digite o valor da base:"))
+    let area = base*altura 
+    return alert ("A area do triangulo é de " + area)
+}
+ calcularAreaRetangulo()
